323b6e82ffef340941567556858bdcfd821794a5
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-10-18 18:44:48 -0400

    update network to be distributed

--- geoDistLRUCache/network.py
+++ geoDistLRUCache/network.py
@@ -1,9 +1,23 @@
 import requests
+import pickle
+import hashlib
 
 class Network:
     def __init__(self, nodes):
         self.nodes = nodes  # A list of URLs for each node in the system
 
     def send_data(self, key, value):
-        for node in self.nodes:
-            requests.post(node, data={'key': key, 'value': value})
+        # Serialize the value with pickle
+        serialized_value = pickle.dumps(value)
+
+        # Determine which node to send the data to based on the key
+        node = self._get_node(key)
+
+        # Send the data to the appropriate node
+        requests.post(node, data={'key': key, 'value': serialized_value})
+
+    def _get_node(self, key):
+        # Use a simple hash of the key to determine the node
+        hash_value = hashlib.sha256(key.encode()).hexdigest()
+        index = int(hash_value, 16) % len(self.nodes)
+        return self.nodes[index]
