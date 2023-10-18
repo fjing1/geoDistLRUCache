2a4e15afc26565b4ea4e02f2e6c71477ec4329d0
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-10-18 16:17:59 -0400

    add LinkedList.py

---
+++ geoDistLRUCache/LinkedList.py
@@ -0,0 +1,51 @@
+from Node import node
+
+class LinkedList:
+    """
+    Linked List. Represents usage history of cache items
+    """
+    head: Optional[Node] = None
+    tail: Optional[Node] = None
+
+    def add_to_head(self, item: Node) -> None:
+        """
+        Add node to the very top of the list
+        """
+        if self.head is not None:
+            item.next = self.head
+            self.head.prev = item
+
+        if self.tail is None:
+            self.tail = item
+
+        self.head = item
+
+    def unlink(self, item: Node) -> None:
+        """
+        Remove references to the node from other nodes on the list
+        """
+        if item is None:
+            return
+
+        prev_item: Node = item.prev
+        next_item: Node = item.next
+
+        # unlink the item node:
+        # link prev and next items
+        # removing referenced to the current item node
+        if prev_item is not None:
+            prev_item.next = next_item
+
+        if next_item is not None:
+            next_item.prev = prev_item
+
+        if self.head == item:
+            # item was the first element in the list
+            self.head = next_item
+        if self.tail == item:
+            # item was the last element in the list
+            self.tail = prev_item
+
+        # make sure that the item itself doesn't have references to other nodes
+        item.prev = None
+        item.next = None
