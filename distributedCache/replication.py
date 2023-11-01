d64d5546f0a78aab9de5521eb885abb3154407b2
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-11-01 19:56:50 -0400

    add consistency in replication.py

--- distributedCache/replication.py
+++ distributedCache/replication.py
@@ -12,6 +12,15 @@ class Replication:
             self.network.send_data(key, value)
             if node != self.primary_node:
                 # Here we assume that `send_data` sends a request to the node
-                # to perform the write operation. You might need to modify this
-                # based on your actual network communication code.
+                # to perform the write operation.
                 self.network.send_data(node, key, value)
+        # As consistency is very important for distributed cacheing or database system
+        # here I conceptually implemented it, for future expansion
+        def check_consistency(self, key):
+            primary_value = self.cache.get(key)
+            for node in self.network.nodes:
+                if node != self.primary_node:
+                    node_value = self.network.get_data(node, key)
+                    if node_value != primary_value:
+                        return False
+            return True
